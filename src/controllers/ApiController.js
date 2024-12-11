6e7f8e220d493654aa25e74325cf91898bdf7514
InzukiOrg <inzuki.nk@gmail.com>
2024-12-11 03:02:20 +0300

    fix(socket): wrong path

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -2,7 +2,6 @@ const AppController = require("./AppController");
 
 class ApiController {
   start_parsing(req, res) {
-    const io = require("../../socket")
     AppController.startParsing();
     res.send("starting..");
   }
